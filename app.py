7a795081e198109ea81d31ada49d90a4ca7ba27e
yjw666123 <160452057+yjw666123@users.noreply.github.com>
2024-02-19 12:00:27 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask,reauest,render_template
+
+app=Flask(_name_)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+  return(render_template("index.html")
+
+  if _name_=="_main_":
+         app.run()
