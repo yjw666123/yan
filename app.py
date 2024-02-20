73bbfbc019372e8fb0055a67e892710b79577a4c
yjw666123 <160452057+yjw666123@users.noreply.github.com>
2024-02-20 14:57:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def main():
 
 @app.route("/imageGPT",methods=["GET","POST"])
 def main():
-  return(render_template("imageGPT.html"r=r))
+  return(render_template("imageGPT.html"))
 
   if __name__=="__main__":
          app.run()
