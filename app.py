a4dc6bf3463130b8a8c1ad0d48bfca6e1de045ab
yjw666123 <160452057+yjw666123@users.noreply.github.com>
2024-02-20 14:46:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ def index():
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-  name=request.form.get("name")
-  return(render_template("main.html",r=name))
+  r=request.form.get("r")
+  return(render_template("main.html",r=r))
 
   if __name__=="__main__":
          app.run()
